4eb9da71b713bae8f361f88466fdb82ac849dc10
Matt <futbolkid19@gmail.com>
2020-01-01 20:03:47 -0500

    Add files via upload

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+export dockerpath="mjpraino/app"
 
 # Step 2:  
 # Authenticate & tag
+docker login --username mjpraino
+docker tag app $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
